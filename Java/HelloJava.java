d80ca7f127f682fe99e88cc9437b67326362fc9c
Stefanene <stefanene@hotmail.com>
2022-07-22 09:56:22 +0300

    added java

---
+++ Java/HelloJava.java
@@ -0,0 +1,10 @@
+package Java;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        String message = "hello to Java world";
+        System.out.println(message);
+        String message2 = "Hello to JAVA world";
+        System.out.println(message2);
+    }
+}
